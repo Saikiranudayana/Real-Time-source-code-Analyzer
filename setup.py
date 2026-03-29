e419a92fa2eb6dcb57502efdc873991aeeb514da
Saikiran Udayana <saikiranudayana@gmail.com>
2026-03-29 16:09:28 +0530

    trail based on noteboooks is started

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Real-Time-Source-code-analyzer",
+    version="0.1.0",
+    author="Sai kiran Udayana",
+    author_email="saikiranudayana@gmail.com",
+    pacakages=find_packages(),
+    install_requires=[]
+)
